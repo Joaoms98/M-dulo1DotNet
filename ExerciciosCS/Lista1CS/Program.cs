efa33bf7f9cb23605173fe3a2039b22f9643f60b
Joaoms98 <jvm1800@gmail.com>
2022-04-04 14:21:04 -0300

    Upload exercicios Cs

---
+++ ExerciciosCS/Lista1CS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciciosCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ExerciciosCS/Lista3CS/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float peso, altura,imc;
+
+            Console.WriteLine("Qual seu peso: ");
+            peso = float.Parse(Console.ReadLine());
+            Console.WriteLine("Qual sua altura: ");
+            altura = float.Parse(Console.ReadLine());
+
+            imc = peso/(altura*altura);
+            Console.WriteLine("Seu ICMC: "+imc);
+        }
+    }
+}
