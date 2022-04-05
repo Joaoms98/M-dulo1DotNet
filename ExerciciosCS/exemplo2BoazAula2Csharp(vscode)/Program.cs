4b90fed85f673ce9d61faa193545696507ee97bc
Joaoms98 <jvm1800@gmail.com>
2022-04-05 12:52:02 -0300

    Upload exemplos aula2

---
+++ ExerciciosCS/exemplo2BoazAula2Csharp(vscode)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exemplo2BoazAula2Csharp_vscode_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com valor vendido mensal: ");
+            double valorvendido = double.Parse(Console.ReadLine());
+            
+            if(valorvendido <=10000)
+            {
+                Console.WriteLine("O valor a receber: R$"+(valorvendido*0.01));
+            }
+            else if(valorvendido<=20000)
+            {
+                Console.WriteLine("O valor a receber: R$"+(valorvendido*0.02));
+            }
+            else if(valorvendido<=30000)
+            {
+                Console.WriteLine("O valor a receber: R$"+(valorvendido*0.03));    
+            }
+            else
+            {
+               Console.WriteLine("O valor a receber: R$"+(valorvendido*0.1)); 
+            }
+        }
+    }
+}
