088869d809d0c01c7b0942cb9a42a10c83f446a2
Joaoms98 <jvm1800@gmail.com>
2022-04-05 13:36:02 -0300

    Upload exercício lista 5CS

---
+++ ExerciciosCS/Lista5CS/Exerciciolista5cs/Exerciciolista5cs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float peso, altura, imc;
+
+
+            Console.WriteLine("Qual seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Qual seu peso: ");
+            peso = float.Parse(Console.ReadLine());
+            Console.WriteLine("Qual sua altura: ");
+            altura = float.Parse(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+
+            if(imc <18.5)
+            {
+                Console.Write("Olá "+nome);
+                Console.Write(" você está abaixo do peso");
+            }
+            else if(imc<25)
+            {
+                Console.Write("Olá " + nome);
+                Console.Write(" você está com o peso normal");
+            }
+            else if(imc<30)
+            {
+                Console.Write("Olá " + nome);
+                Console.Write(" você está acima do peso");
+            }
+            else if(imc>=30)
+            {
+                Console.Write("Olá " + nome);
+                Console.Write(" você está com obesidade");
+            }
+        }
+    }
+}
