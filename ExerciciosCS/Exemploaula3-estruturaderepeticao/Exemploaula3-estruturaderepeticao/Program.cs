918e34f0764a8de2451b3b7a037f5c70bbe33af1
Joaoms98 <jvm1800@gmail.com>
2022-04-06 12:44:40 -0300

    Upload exemplos aula 3 estrutura de repetição

---
+++ ExerciciosCS/Exemploaula3-estruturaderepeticao/Exemploaula3-estruturaderepeticao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Exemploaula3_estruturaderepeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+            for(int i=1;i<=20;i++)
+            {
+                Console.WriteLine($"Posição {i} Entre com o valor se deseja somar: ");
+                valor += int.Parse(Console.ReadLine());
+   //valor += i; economiza o código coloca isso na linha 14
+            }
+            Console.WriteLine($"Somatoria: {valor}");
+   //interpolação colocando cifrão antes da string, para add var colocar entre chaves
+        }
+    }
+}
+
---
+++ ExerciciosCS/Exemploaula3-ex2 estruturaderepeticao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exemploaula3_ex2_estruturade_repeticao_vscode_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada");
+            for(int x=0;x<=10;x++)
+            {
+                Console.WriteLine($"Tabuada do: {+x}");
+                for(int y=1;y<=10;y++)
+                {
+                    Console.WriteLine($"{x}x{y}={x*y}");
+                }
+            }
+        }
+    }
+}
