fa344f152bdcf0e5af9fecae2d4fd9e5fc10cde2
Joaoms98 <jvm1800@gmail.com>
2022-04-06 13:17:26 -0300

    Upload exercício lista6

---
+++ ExerciciosCS/Lista6CS/Lista6CS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lista6CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe qual tabuada gostaria de saber:  ");
+            int x=int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Tabuada do: {+x}");
+                for (int y = 1; y <= 10; y++)
+                {
+                    Console.WriteLine(x + "x" + y + "=" + x * y);
+                }
+        }
+    }
+}
