c41219aa212f8df4e000f19d36babbdbcb416d16
Joaoms98 <jvm1800@gmail.com>
2022-04-07 14:56:59 -0300

    Upload exercicios functions

---
+++ ExerciciosCS/Lista7CS/Lista7CS/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Lista7CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora");
+            Console.WriteLine("Pressione I para iniciar");
+            string inicializador = Console.ReadLine();
+
+            while(inicializador=="I")
+            {
+                Console.Write("Digite o primeiro número: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o operador: ");
+                string operador = Console.ReadLine();
+
+                Console.Write("Digite o segundo número: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                switch (operador)
+                {
+                    case "+":
+                        Console.WriteLine($"Resultado:{num1 + num2}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"Resultado:{num1 - num2}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"Resutado:{num1 * num2}");
+                        break;
+                    case "/":
+                        Console.WriteLine($"Resultado:{num1 / num2}");
+                        break;
+                    default:
+                        Console.WriteLine("operador invalido");
+                        break;
+                }
+                Console.WriteLine("Pressione E para sair ou qualquer letra para continuar");
+                string finalizador = Console.ReadLine();
+
+                if(finalizador=="E")
+                {
+                    break;
+                }
+            } 
+        }
+    }
+}
