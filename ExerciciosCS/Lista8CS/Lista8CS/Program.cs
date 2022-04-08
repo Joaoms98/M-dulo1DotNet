ea59ea0f32cd80100839f1a3a8b27beef9f5373c
Joaoms98 <jvm1800@gmail.com>
2022-04-08 16:06:46 -0300

    Upload exercicio lista8

---
+++ ExerciciosCS/Lista8CS/Lista8CS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Lista8CS.src;
+
+
+namespace Lista8CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            violao Giannini = new violao("Giannini", "castanho claro", 1200);   // informar os valores dos atributos
+            violao Tagima = new violao("Tagima", "preto", 800);
+            violao Digiorgi = new violao("Digiorgi", "castanho escuro", 1800);
+
+            Console.WriteLine($"violao {Giannini.Name} é {Giannini.Color} e custa {Giannini.Preco}");
+            Giannini.sound("Seco");
+            Console.WriteLine($"nos acordes: {Giannini.acordes()}");
+        }
+    }
+}
---
+++ ExerciciosCS/Lista8CS/Lista8CS/src/violao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lista8CS.src
+{
+    internal class violao
+    {
+        //atributos
+        public string Name,Color;
+        public double Preco;
+        //construtor
+        public violao(string _name,string _color,double _preco)
+        {
+            Name = _name;
+            Color = _color;
+            Preco = _preco;
+        }
+        //método
+        public void sound (string sound)  //pode fazer assimmmmm. na hora de chamar eu chamo ele pelo nome memo Giannini.sound("Seco");
+        {
+            Console.WriteLine($"O {Name} tem um som {sound}");
+        }
+        public string acordes() // ou assimmm.Na hora de chamar eu chamo Console.WriteLine(Giannini.acordes());
+        {
+            return "C#,A#-";
+        }
+    }
+}
