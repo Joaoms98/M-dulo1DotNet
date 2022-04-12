003787f4f4f1b35d7b0ab9f5b954d0d13e8c0e8c
Joaoms98 <jvm1800@gmail.com>
2022-04-11 21:02:47 -0300

    Upload aula heranca

---
+++ ExerciciosCS/ExemploAulaBoazHerança1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ExemploAulaBoazHerança1.src;
+
+namespace ExemploAulaBoazHerança1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora");
+            
+            Calc objeto = new Calc();
+
+            Console.WriteLine($"Soma: {objeto.Somar(1, 2)}");
+            Console.WriteLine($"Subtração: {objeto.Subtrair(1, 2)}");
+            Console.WriteLine($"Multiplicação: {objeto.Multiplicar(1, 2)}");
+            Console.WriteLine($"Divisão: {objeto.Dividir(1, 2)}");
+        }
+    }
+}
