04074f90b55ea79a72c66b8c819a80242f9a090a
Joaoms98 <jvm1800@gmail.com>
2022-04-12 17:02:17 -0300

    upload exercicios aula11

---
+++ ExerciciosCS/Aula11BoazListExemplos/Aula11BoazListExemplos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula11BoazListExemplos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista");
+
+            //construção da lista
+            List<string> lista = new List<string>();
+
+            // lista feita vai aparecer o output yasmin...
+            lista.Add("Yasmin");
+            lista.Add("Karol");
+            lista.Add("Karol");
+            lista.Add("Correia");
+            lista.Add("Kaique");
+
+            //esse for é para colocar um input para o usuário inserir os dados
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Entre com o nome: ");
+                var nome = Console.ReadLine();
+                lista.Add(nome);
+            }
+            Console.WriteLine("lista:");
+
+            //para percorrer minha lista
+            foreach (string item in lista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ ExerciciosCS/Lista10CsExercicio/Lista10(2)Cs/Tamanho.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista10_2_Cs
+{
+    internal interface Tamanho
+    {
+        double AlturaBase(double Atura, double Base);
+    }
+}
