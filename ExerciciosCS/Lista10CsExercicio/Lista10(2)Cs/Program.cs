f04af8a7784d441b3882fff930130213a624bdba
Joaoms98 <jvm1800@gmail.com>
2022-04-12 17:43:01 -0300

    Upload exercicios aula11

--- ExerciciosCS/Lista10CsExercicio/Lista10(2)Cs/Program.cs
+++ ExerciciosCS/Lista10CsExercicio/Lista10(2)Cs/Program.cs
@@ -6,7 +6,11 @@ namespace Lista10_2_Cs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Vamos calcular a área de uma figura geométrica inserindo os dados 1,70 para altura e 1,10 para a base");
+            
+            Area area = new Area();
+
+            Console.WriteLine($"A área  é: {area.AlturaBase(1.70,1.10)}");
         }
     }
 }
